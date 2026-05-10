4cc3903d920beb9b21f65d1ff23f2eef7fbf384e
edena <edenaong13@gmail.com>
2026-05-10 21:06:29 +0800

    added get_match_result, goal_diff and squad_rating functions to features

---
+++ src/features.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+
+def get_match_result(row):
+    if row["home_score"] > row["away_score"]:
+        return "home_win"
+    elif row["home_score"] < row["away_score"]:
+        return "away_win"
+    else:
+        return "draw"
+
+def goal_diff(row):
+    return row["home_score"] - row["away_score"]
+
+position_mapping = {
+    "GK": "GK",
+    "LB": "DEF", 
+    "CB": "DEF", 
+    "RB": "DEF",
+    "CDM": "MID", 
+    "CM": "MID",
+    "CAM": "MID",
+    "LM": "MID", 
+    "RM": "MID",
+    "LW": "FWD", 
+    "RW": "FWD",
+    "ST": "FWD"
+}
+
+def squad_rating(players):
+    
+    squad_requirements = {
+        "GK": 3,
+        "DEF": 9,
+        "MID": 9,
+        "FWD": 5
+    }
+
+    team_ratings = []
+
+    for country in players["nationality"].unique():
+        country_players = players[players["nationality"] == country]
+        selected_players = []
+
+        for pos, n_players in squad_requirements.items():
+            pos_players = country_players[country_players["position"] == pos]
+            if len(pos_players) == 0:
+                continue
+            elif len(pos_players) != 0 and len(pos_players) < n_players:
+                top_players = pos_players
+            else:
+                top_players = pos_players.nlargest(n_players, "overall")
+            selected_players.append(top_players)
+
+        squad = pd.concat(selected_players)
+        squad_rating = squad["overall"].mean()
+
+        team_ratings.append({"country": country, "squad_rating": squad_rating, "squad_size": len(squad)})
+
+    return pd.DataFrame(team_ratings)
